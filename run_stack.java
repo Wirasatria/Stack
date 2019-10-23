a4622bfa5a52e9d1d67d986af4c1d880a7a73f56
WIRASATRIA PUTRA <56837285+Wirasatria@users.noreply.github.com>
2019-10-23 22:26:14 +0700

    Add files via upload

---
+++ run_stack.java
@@ -0,0 +1,24 @@
+package stack_tugas;
+
+
+public class run_stack {
+  
+    public static void main(String[] args) {
+        stack x = new stack(7);
+
+        x.push("Kemeja");
+        x.push("Kaos");
+        x.push("Koko");
+        x.push("Jubah");
+        x.push("Jaket");
+        x.push("Singlet");
+        x.push("Sweater");
+
+        x.cetak();
+
+        System.out.println("========================================");
+        System.out.println(x.cari("Jaket"));
+    }
+}
+
+
---
+++ stack.java
@@ -0,0 +1,55 @@
+package stack_tugas;
+
+  import java.sql.SQLOutput;
+public class stack {
+  
+    String data[];
+    int top;
+
+    public stack(int jmlh) {
+        data = new String[jmlh];
+        top = -1;
+    }
+
+    public void push(String Pakaian){
+        if(top < data.length-1){
+            top++;
+            data[top] = Pakaian;
+        } else{
+            System.out.println("Lemari anda Sudah Penuh");
+        }
+    }
+
+    public String pop(){
+        if (top >= 0){
+            String temp = data[top];
+            top--;
+            return temp;
+        } else{
+            System.out.println("Lemari anda Kosong");
+        }
+        return "";
+    }
+
+    public void cetak(){
+        System.out.println(" Rak lemari Pakaian");
+        System.out.println("=====================================");
+        for (int i = data.length-1; i >= 0  ; i--) {
+            System.out.println(data[i] + "");
+        }
+    }
+
+    public String cari(String baju){
+        System.out.println("Mencari  : " + baju);
+        for (int i = data.length-1; i >= 0; i--) {
+            if (data[i] == baju){
+                return "ada di rak lemari ke " + (i+1);
+            }
+        }
+        return "Belum dicuci";
+    }
+
+
+}
+
+
